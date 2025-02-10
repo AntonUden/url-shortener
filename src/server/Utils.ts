db5043a1225c3d6f2c5e9fedb04edb723276cf6b
Anton Udén <udenanton@hotmail.com>
2025-02-10 12:27:44 +0100

    Remove some characters from url that can be confused with others

--- src/server/Utils.ts
+++ src/server/Utils.ts
@@ -1,6 +1,6 @@
 export default class Utils {
 	public static randomString(length: number): string {
-		let chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		let chars = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghkmnpqrstuvwxyz123456789';
 		let result = '';
 		for (let i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
 		return result;
